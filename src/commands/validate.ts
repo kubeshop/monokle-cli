9f9e8e2bc44aad534b1826ffe3e9d56c031fba08
Delnat Wito <wito.delnat@gmail.com>
2022-10-12 15:10:17 +0200

    feat: read directory from CLI

--- src/commands/validate.ts
+++ src/commands/validate.ts
@@ -4,8 +4,11 @@ import {
   readConfig,
   ResourceParser,
 } from "@monokle/validation";
-import { readFile } from "fs/promises";
+import { lstatSync } from "fs";
+import { readFile as readFileFromFs } from "fs/promises";
+import glob from "tiny-glob";
 import { command } from "../utils/command.js";
+import chunkArray from "lodash/chunk.js";
 import { extractK8sResources, File } from "../utils/extract.js";
 import { print } from "../utils/screens.js";
 import { streamToPromise } from "../utils/stdin.js";
@@ -35,13 +38,8 @@ export const validate = command<Options>({
       .positional("path", { type: "string", demandOption: true });
   },
   async handler({ path, output, config: configPath }) {
-    const content = await readContent(path);
-    const file: File = {
-      id: path === "" ? "stdin" : path,
-      content,
-      path: path === "" ? "stdin" : path,
-    };
-    const resources = extractK8sResources([file]);
+    const files = await readFiles(path);
+    const resources = extractK8sResources(files);
 
     const parser = new ResourceParser();
 
@@ -69,11 +67,67 @@ export const validate = command<Options>({
   },
 });
 
-async function readContent(path: string): Promise<string> {
-  if (path === "") {
-    const buffer = await streamToPromise(process.stdin);
-    const content = buffer.toString("utf8");
-    return content;
+async function readFiles(path: string): Promise<File[]> {
+  if (isStdinLike(path)) {
+    const stdin = await readStdin();
+    return [stdin];
+  } else if (isFileLike(path)) {
+    const file = await readFile(path);
+    return [file];
+  } else {
+    return readDirectory(path);
   }
-  return readFile(path, "utf8");
+}
+
+function isFileLike(path: string) {
+  return lstatSync(path).isFile();
+}
+
+async function readFile(path: string): Promise<File> {
+  const content = await readFileFromFs(path, "utf8");
+
+  return {
+    id: path,
+    path,
+    content,
+  };
+}
+
+function isStdinLike(path: string) {
+  return path === "";
+}
+
+async function readStdin(): Promise<File> {
+  const buffer = await streamToPromise(process.stdin);
+  const content = buffer.toString("utf8");
+
+  return {
+    id: "stdin",
+    path: "stdin",
+    content,
+  };
+}
+
+async function readDirectory(directoryPath: string): Promise<File[]> {
+  const filePaths = await glob(`${directoryPath}/**/*.{yaml,yml}`);
+  const files: File[] = [];
+
+  for (const chunk of chunkArray(filePaths, 5)) {
+    const promise = await Promise.allSettled(
+      chunk.map((path) => {
+        return readFileFromFs(path, "utf8").then(
+          (content): File => ({ id: path, path, content })
+        );
+      })
+    );
+
+    for (const result of promise) {
+      if (result.status === "rejected") {
+        continue;
+      }
+      files.push(result.value);
+    }
+  }
+
+  return files;
 }
