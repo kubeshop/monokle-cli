8384b16e52464907acf67991cc7d8722e4b027a8
Hamid <33328203+emamihe@users.noreply.github.com>
2023-03-17 14:50:35 +0300

    Create monokle-cli.rb

---
+++ manifest/monokle-cli.rb
@@ -0,0 +1,18 @@
+class MonokleCli < Formula
+  desc "Monokle CLI analyzes your Kubernetes resources to quickly find misconfigurations."
+  homepage "https://github.com/kubeshop/monokle-core/tree/main/packages/cli"
+  license "MIT"
+
+  version "VERSION"
+  if Hardware::CPU.arm?
+    url "ARM_URL"
+    sha256 "ARM_SHA"
+  else
+    url "AMD_URL"
+    sha256 "AMD_SHA"
+  end
+
+  def install
+    bin.install "monokle"
+  end
+end
