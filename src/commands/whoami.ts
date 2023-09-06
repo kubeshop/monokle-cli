6268f15261275d768f8544454156e6d9f7c842a9
f1ames <dr.odpowiedz@gmail.com>
2023-09-06 12:11:29 +0200

    fix: improve 'whoami' output

--- src/commands/whoami.ts
+++ src/commands/whoami.ts
@@ -1,8 +1,8 @@
 import { authenticatorGetter } from "../utils/authenticator.js";
-import { success } from "./whoami.io.js";
+import { error, success } from "./whoami.io.js";
 import { command } from "../utils/command.js";
 import { print } from "../utils/screens.js";
-import { throwIfNotAuthenticated } from "../utils/conditions.js";
+import { isAuthenticated } from "../utils/conditions.js";
 
 type Options = {};
 
@@ -10,9 +10,11 @@ export const whoami = command<Options>({
   command: "whoami",
   describe: "Get info about current user",
   async handler() {
-    throwIfNotAuthenticated();
-
-    const authenticator = authenticatorGetter.authenticator;
-    print(success(authenticator.user.email!));
+    if (!isAuthenticated()) {
+      print(error);
+    } else {
+      const authenticator = authenticatorGetter.authenticator;
+      print(success(authenticator.user.email!));
+    }
   },
 });
