daa805bca31efe65fee61a2d2d5ec4e779119a8e
f1ames <dr.odpowiedz@gmail.com>
2023-11-17 12:05:36 +0100

    chore: add origin precedence explanation

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -16,6 +16,13 @@ export class StorageSettings extends StorageHandler<Settings> {
   }
 
   get origin() {
+    // Origin precedence:
+    // 1. Origin passed via `--origin` flag (which is stored in `currentData`).
+    // 2. MONOKLE_ORIGIN environment variable.
+    // 3. Origin stored in `initialData` (so the one saved on login if session is still valid).
+    // 4. Default origin (Monokle Cloud SaaS).
+    //
+    // This means even if user is logged in, `--origin` flag or env var can still be used to override the stored origin.
     return this.currentData.origin || process.env.MONOKLE_ORIGIN || this.initialData.origin || DEFAULT_ORIGIN;
   }
 
